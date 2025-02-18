b3f9855bbf48a5228a84ce11b9a3459a018660ac
jostar222 <whtjdgus911@gmail.com>
2025-02-18 23:22:02 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState(0);
+  const [dollar, setdollar] = useState(0);
+
+  const selectCoin  = ((event) => setCoin(event.target.value));
+  const myDollar = ((event) => setdollar(event.target.value));
+  useEffect(() => {
+      fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(coin);
+  console.log(dollar);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input value={toDo} onChange={onChange} type="text" placeholder='Write your to do...'/>      
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={selectCoin}>
+          {coins.map((coin) => 
+            <option value={coin.quotes.USD.price} key={coin.id}>{coin.name} ({coin.symbol}) : ${(coin.quotes.USD.price).toFixed(2)}
+            </option>
+          )}
+        </select>)}
+      <div>
+      <hr/>
+
+      <div>
+        <input type="number" value={dollar} onChange={myDollar} placeholder="WRITE UR USD"></input>
+      </div>
+        <h2>Buy {dollar / coin}</h2>
+      </div>
     </div>
   );
 }
